e7bf0f5c4bf268442c32f1fa50d673dfec717b3c
va-run-6626 <vbhutada143@gmail.com>
2023-08-06 17:15:52 +0530

    minor changes

--- src/DSA_1_B1_May/Session2/Prog4_CyclicRotation.java
+++ src/DSA_1_B1_May/Session2/Prog4_CyclicRotation.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class Prog4_CyclicRotation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];
-        System.out.println("Enter elements in the array of length 5 : ");
-        for(int i = 0; i < arr.length; i++){
+        System.out.println("Enter the size of array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements in the array of length "+ n+ " :");
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
         System.out.println("Enter number of times it should be rotated : ");
